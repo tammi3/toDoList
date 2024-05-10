c53255d18a192898e55696debbd2fe1dc9a2839a
tamaraekiye <113463514+tammi3@users.noreply.github.com>
2024-05-10 16:13:51 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 new Vue({
-    el: '#vue-app',
-    data:{
-        activity:'',
-        toDoItems:[]
+  el: "#vue-app",
+  data: {
+    completed: "",
+    todo: {
+      task: "",
+      date: "",
+      time: "",
     },
-    methods:{
-        addItem: function(){
-            if (this.activity !='') {
-                this.toDoItems.push(this.activity);
-                this.activity ='';
-            }
-        },
-        removeItem: function(index){
-            this.toDoItems.splice(index,1);
-        }
-    }
-})
+    toDos: [],
+    completedToDos: [],
+    
+  },
+  methods: {
+    addTask: function () {
+      if (
+        this.todo.task != "" &&
+        this.todo.date != "" &&
+        this.todo.time != ""
+      ) {
+        this.toDos.push(this.todo);
+        this.todo = {
+          task: "",
+          date: "",
+          time: "",
+        };
+      }
+    },
+    removeTask: function (index) {
+      this.toDos.splice(index, 1);
+    },
+    completedTask: function (index) {
+      this.completedToDos.push(this.toDos[index])
+      this.toDos.splice(index, 1);
+    },
+  },
+});
